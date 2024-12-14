028e80e97f6a62a194db8ab666d112538ad20d71
Tsumihara <155585876+Tsumihara@users.noreply.github.com>
2024-12-14 07:30:51 -0500

    Update secedit.ps1

--- secedit.ps1
+++ secedit.ps1
@@ -1,4 +1,5 @@
-Invoke-WebRequest “https://raw.githubusercontent.com/Tsumihara/PowerShell/refs/heads/main/secedit.cfg” -OutFile C:\secedit.cfg
+$secedit = “https://raw.githubusercontent.com/Tsumihara/PowerShell/refs/heads/main/secedit.cfg”
+Invoke-WebRequest $secedit -OutFile C:\secedit.cfg
 secedit /configure /db c:\windows\security\local.sdb /cfg "C:\secedit.cfg"
 Set-ExecutionPolicy RemoteSigned
 Install-Module -Name PSWindowsUpdate -Force
